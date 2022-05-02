8ea72b70afff6d2c542d329fc7e791a0d536cd26
Ashutosh Sahu <ashutoshsahu@iitbhilai.ac.in>
2022-05-02 12:20:05 +0530

    add communication over lan

--- main.py
+++ main.py
@@ -59,4 +59,5 @@ def unlock(bike_id):
 if __name__ == '__main__':
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    # app.run()
+    app.run(host="0.0.0.0")
